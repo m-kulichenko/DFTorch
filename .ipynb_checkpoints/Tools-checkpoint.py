e9d14c939af855373dbc4b8e4d647b49dbee86fa
kulichenko-LANL <maxim@lanl.gov>
2026-02-21 23:18:31 -0700

    Stop tracking .vscode

--- .ipynb_checkpoints/Tools-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import torch
-def fractional_matrix_power_symm(A, power):
-    print('  Doing fractional_matrix_power_symm')
-    # Symmetric fractional matrix power using eigendecomposition
-    eigvals, eigvecs = torch.linalg.eigh(A)
-    eigvals_clamped = torch.clamp(eigvals, min=1e-12)
-    D_power = torch.diag(eigvals_clamped ** power)
-    return eigvecs @ D_power @ eigvecs.T
